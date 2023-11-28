eb186d2faec2e8719a790070b15c9e8e74961d03
signor1 <emmanuelomemgboji@gmail.com>
2023-11-28 14:56:18 +0100

    introduction

---
+++ js/main.js
@@ -0,0 +1,46 @@
+document.getElementsByTagName("h1")[0].innerHTML = "Welome";
+
+// let animals = ["cat", "dog", "horse"];
+const sum = (a, b) => a + b;
+
+const minus = (a, b) => a - b;
+
+function multiply(a, b) {
+  return a * b;
+}
+
+let a = 10;
+const b = 20;
+
+var salary = 100000;
+var utilityBills = 20000;
+var tax = (salary * 2) / 100;
+
+var moneyR = salary - (tax + utilityBills);
+
+console.log(moneyR);
+
+a = 20;
+
+// automatic
+x = 20;
+y = 50;
+z = x + y;
+console.log(z);
+
+let =
+  // length, index;
+  document.getElementsByTagName("h2")[0].innerHTML = multiply(212, 56);
+// 'cat'
+
+// '10'
+
+// 10
+
+// null
+
+// undefined
+
+// true || false
+
+// { name: "Emma", email: "emma@gmail.com", age: 12, isOnline: false }
